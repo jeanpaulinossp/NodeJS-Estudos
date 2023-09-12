b1863e5b5bf581b17a1199597ce3b2f7e7e8fe5d
Jean Paulino <jeanpaulino.ssp@gmail.com>
2023-09-12 17:26:16 -0300

    att

--- ProjetoPerguntasRespostas/index.js
+++ ProjetoPerguntasRespostas/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
 
-// Database
+// Database Sequelize
 connection
   .authenticate()
   .then(() => {
-    console.log("Conexão feito com sucesso!");
+    console.log("Conexão feita com sucesso!");
   })
   .catch((msgErro) => {
     console.log(msgErro);
@@ -25,7 +27,12 @@ app.use(bodyParser.json());
 
 // roteamento
 app.get("/", (req, res) => {
-  res.render("index");
+  // o raw é para trazer somente os dados da tabela
+  Pergunta.findAll({ raw: true, order: [["id", "DESC"]] }).then((perguntas) => {
+    res.render("index", {
+      perguntas: perguntas,
+    });
+  });
 });
 
 app.get("/perguntar", (req, res) => {
@@ -35,7 +42,45 @@ app.get("/perguntar", (req, res) => {
 app.post("/salvarpergunta", (req, res) => {
   var titulo = req.body.titulo;
   var descricao = req.body.descricao;
-  res.send(`${titulo} e ${descricao}`);
+  // o create é para salvar a pergunta no BD
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao,
+  }).then(() => {
+    res.redirect("/");
+  });
+});
+
+app.get("/pergunta/:id", (req, res) => {
+  var id = req.params.id;
+  Pergunta.findOne({
+    where: { id: id },
+  }).then((pergunta) => {
+    if (pergunta !== undefined) {
+      Resposta.findAll({
+        where: { perguntaId: pergunta.id },
+        order: [["id", "DESC"]],
+      }).then((respostas) => {
+        res.render("pergunta", {
+          pergunta: pergunta,
+          respostas: respostas,
+        });
+      });
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+app.post("/responder", (req, res) => {
+  var corpo = req.body.corpo;
+  var perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId,
+  }).then(() => {
+    res.redirect("/pergunta/" + perguntaId);
+  });
 });
 
 app.listen(8080, () => {
