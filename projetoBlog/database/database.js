5c9f4f06eb46c5a01d240d55beaa3f07c039bb87
Jean Paulino <jeanpaulino.ssp@gmail.com>
2023-10-03 13:26:43 -0300

    Criação da tela de artigos

--- projetoBlog/database/database.js
+++ projetoBlog/database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize("guiablog", "root", "a123456*", {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00",
 });
 
 module.exports = connection;
--- projetoBlog/routes/ArticlesRoutes.js
+++ projetoBlog/routes/ArticlesRoutes.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const routerArticles = express.Router();
+const Category = require("../categories/Category");
+const Article = require("../articles/Article");
+const slugify = require("slugify");
 
-routerArticles.get("/articles", (req, res) => {
+routerArticles.get("/admin/articles", (req, res) => {
   res.send("ROTA DE ARTIGOS");
 });
 
 routerArticles.get("/admin/articles/new", (req, res) => {
-  res.send("ROTA PARA CRIAR UM NOVO ARTIGO");
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories: categories });
+  });
+});
+
+routerArticles.post("/articles/save", (req, res) => {
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category,
+  }).then(() => {
+    res.redirect("/admin/articles");
+  });
 });
 
 module.exports = routerArticles;
