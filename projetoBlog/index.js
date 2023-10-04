d55d6274f10156ae5b143743e0765669d4659383
Jean Paulino <jeanpaulino.ssp@gmail.com>
2023-10-04 13:25:03 -0300

    att

--- projetoBlog/index.js
+++ projetoBlog/index.js
@@ -34,7 +34,30 @@ app.use("/", categoriesRoutes);
 app.use("/", articlesRoutes);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  Article.findAll({
+    order: [["id", "DESC"]],
+  }).then((articles) => {
+    res.render("index", { articles: articles });
+  });
+});
+
+app.get("/:slug", (req, res) => {
+  var slug = req.params.slug;
+  Article.findOne({
+    where: {
+      slug: slug,
+    },
+  })
+    .then((article) => {
+      if (article !== undefined) {
+        res.render("article", { article: article });
+      } else {
+        res.redirect("/");
+      }
+    })
+    .catch((err) => {
+      res.redirect("/");
+    });
 });
 
 app.listen(3001, () => {
--- projetoBlog/routes/ArticlesRoutes.js
+++ projetoBlog/routes/ArticlesRoutes.js
@@ -5,7 +5,11 @@ const Article = require("../articles/Article");
 const slugify = require("slugify");
 
 routerArticles.get("/admin/articles", (req, res) => {
-  res.send("ROTA DE ARTIGOS");
+  Article.findAll({
+    include: [{ model: Category }],
+  }).then((articles) => {
+    res.render("admin/articles/index", { articles: articles });
+  });
 });
 
 routerArticles.get("/admin/articles/new", (req, res) => {
@@ -27,6 +31,25 @@ routerArticles.post("/articles/save", (req, res) => {
   }).then(() => {
     res.redirect("/admin/articles");
   });
+});
+
+routerArticles.post("/articles/delete", (req, res) => {
+  var id = req.body.id;
+  if (id !== undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/articles");
+      });
+    } else {
+      res.redirect("/admin/articles");
+    }
+  } else {
+    res.redirect("/admin/articles");
+  }
 });
 
 module.exports = routerArticles;
