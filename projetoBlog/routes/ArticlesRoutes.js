83b41fc06284f9261a4fbe40e9eab4d6f0352ac8
Jean Paulino <jeanpaulino.ssp@gmail.com>
2023-10-09 10:49:36 -0300

    att

--- projetoBlog/routes/ArticlesRoutes.js
+++ projetoBlog/routes/ArticlesRoutes.js
@@ -69,4 +69,58 @@ routerArticles.get("/admin/articles/edit/:id", (req, res) => {
     });
 });
 
+routerArticles.post("/articles/update", (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.update(
+    {
+      title: title,
+      body: body,
+      categoryId: category,
+      slug: slugify(title),
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  )
+    .then(() => {
+      res.redirect("/admin/articles");
+    })
+    .catch((err) => {
+      res.redirect("/");
+    });
+});
+
+routerArticles.get("/articles/page/:num", (req, res) => {
+  var page = Number(req.params.num);
+  var offset = 0;
+
+  if (isNaN(page) || page === 1) {
+    offset = 0;
+  } else {
+    offset = page * 4;
+  }
+
+  Article.findAndCountAll({
+    limit: 4,
+    offset: offset,
+  }).then((articles) => {
+    var next;
+    if (offset + 4 >= articles.count) {
+      next = false;
+    } else {
+      next = true;
+    }
+    var result = { next: next, articles: articles };
+    Category.findAll().then((categories) => {
+      res.render("admin/articles/page", { result: result, categories: categories });
+    });
+  });
+});
+
 module.exports = routerArticles;
