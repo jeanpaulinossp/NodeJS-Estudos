74461fe3bfdf6352580af0c9db7ad287e1035308
Jean Paulino <jeanpaulino.ssp@gmail.com>
2023-10-27 17:58:55 -0300

    att

--- ProjetoConversorExcel/app.js
+++ ProjetoConversorExcel/app.js
@@ -1,10 +1,17 @@
 let Reader = require("./Reader");
+let Processor = require("./Processor");
+let Table = require("./Table");
+let HtmlParser = require("./HtmlParser");
 
 let leitor = new Reader();
 
 async function main() {
   let dados = await leitor.Read("./users.csv");
-  console.log(dados);
+  let dadosProcessador = Processor.Process(dados);
+  let usuarios = new Table(dadosProcessador);
+  let html = await HtmlParser.Parse(usuarios);
+
+  console.log(html);
 }
 
 main();
