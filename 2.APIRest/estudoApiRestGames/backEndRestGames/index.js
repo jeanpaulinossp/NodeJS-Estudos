0bc43b12e27142071ca27f0558e75f191fc35d9c
Jean Paulino <jeanpaulino.ssp@gmail.com>
2023-11-03 10:32:28 -0300

    Insercao de autenticacao JWT na API

--- 2.APIRest/estudoApiRestGames/backEndRestGames/index.js
+++ 2.APIRest/estudoApiRestGames/backEndRestGames/index.js
@@ -1,12 +1,36 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+
+const JWTSecret = "mwdqioahd84234iu308a556915.++-.5djsidjksasasae2ytr43yuwp||zzasq;.,][ççpç";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+function auth(req, res, next) {
+  const authToken = req.headers["authorization"];
+  if (authToken !== undefined) {
+    const bearer = authToken.split(" ");
+    const token = bearer[1];
+    jwt.verify(token, JWTSecret, (err, data) => {
+      if (err) {
+        res.json({ err: "Token inválido." });
+        res.sendStatus(401).end();
+      } else {
+        req.token = token;
+        req.loggedUser = { id: data.id, email: data.email };
+        next();
+      }
+    });
+  } else {
+    res.json({ err: "Token inválido." });
+    res.sendStatus(401).end();
+  }
+}
 
 const DB = {
   games: [
@@ -29,10 +53,24 @@ const DB = {
       price: 20,
     },
   ],
+  users: [
+    {
+      id: 1,
+      name: "Jean Carlos",
+      email: "jean.ssparaiso@gmail.com",
+      password: "nodejs@1",
+    },
+    {
+      id: 2,
+      name: "Jean Paulino",
+      email: "jeanpaulino.ssp@gmail.com",
+      password: "nodejs@2",
+    },
+  ],
 };
 
-app.get("/games", (req, res) => {
-  res.json(DB.games);
+app.get("/games", auth, (req, res) => {
+  res.json({ user: req.loggedUser, games: DB.games });
 });
 
 app.get("/game/:id", (req, res) => {
@@ -103,6 +141,40 @@ app.put("/game/:id", (req, res) => {
     } else {
       res.sendStatus(404).end();
     }
+  }
+});
+
+app.post("/auth", (req, res) => {
+  let { email, password } = req.body;
+  if (email !== undefined) {
+    const user = DB.users.find((user) => user.email === email);
+
+    if (user !== undefined) {
+      if (user.password === password) {
+        jwt.sign(
+          { id: user.id, email: user.email },
+          JWTSecret,
+          { expiresIn: "24h" },
+          (err, token) => {
+            if (err) {
+              res.json({ err: "Falha interna." });
+              res.sendStatus(401).end();
+            } else {
+              res.json({ token: token });
+            }
+          }
+        );
+      } else {
+        res.json({ err: "Credenciais inválidas." });
+        res.sendStatus(401).end();
+      }
+    } else {
+      res.json({ err: "O email enviado não existe." });
+      res.sendStatus(404).end();
+    }
+  } else {
+    res.json({ err: "O email enviado é inválido." });
+    res.sendStatus(400).end();
   }
 });
 
