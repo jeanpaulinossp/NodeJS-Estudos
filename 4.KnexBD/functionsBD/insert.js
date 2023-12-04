0a9459105769374ee9b93512f529d325082a3ecc
Jean Paulino <jean.ssparaiso@gmail.com>
2023-12-04 19:47:55 -0300

    att

--- 4.KnexBD/functionsBD/insert.js
+++ 4.KnexBD/functionsBD/insert.js
@@ -31,3 +31,17 @@ database
   .catch((err) => {
     console.log(err);
   });
+
+// INSERT ASSOCIATIONS
+database
+  .insert({
+    nome: "Blizzard",
+    game_id: 12,
+  })
+  .table("estudios")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- 4.KnexBD/functionsBD/join.js
+++ 4.KnexBD/functionsBD/join.js
@@ -1,12 +1,56 @@
-// INSERT ASSOCIATIONS
+// unir em uma consulta várias tabelas
+
+// left join e rightjoin retorna os resultados mesmo que não tenha relacionamento
+// innerJoin retorna os resultados com relacionamento e desconsidera os nulos
+// o as serve para trazer a resposta com um name diferente. Usado quando há nome de tabelas iguais
 database
-  .insert({
-    nome: "Blizzard",
-    game_id: 12,
-  })
-  .table("estudios")
+  .select(["games.*", "estudios.nome as estudio.nome"])
+  .table("games")
+  .innerJoin("estudios", "estudios.game_id", "games.id")
   .then((data) => {
     console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// join com where
+database
+  .select(["games.*", "estudios.nome as estudio.nome"])
+  .table("games")
+  .innerJoin("estudios", "estudios.game_id", "games.id")
+  .where("games.id", 12)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// join de um para vários com lógica para trazer os dados de forma usável
+database
+  .select(["games.*", "estudios.nome as estudio_nome"])
+  .table("games")
+  .innerJoin("estudios", "estudios.game_id", "games.id")
+  .then((data) => {
+    let gamesArray = [];
+
+    data.forEach((row) => {
+      let existingGame = gamesArray.find((game) => game.id === row.id);
+      if (!existingGame) {
+        existingGame = {
+          id: row.id,
+          nome: row.nome,
+          estudios: [],
+        };
+        gamesArray.push(existingGame);
+      }
+      if (row.nome) {
+        existingGame.estudios.push({ nome: row.estudio_nome });
+      }
+    });
+
+    console.dir(gamesArray, { depth: null });
   })
   .catch((err) => {
     console.log(err);
